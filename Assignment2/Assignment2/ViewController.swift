21846b6c0a691b2fc42746c14827523719517c74
milkaMA <Micah@Micahs-MacBook-Pro.local>
2016-07-04 20:12:02 -0400

    Fixed problem 2

--- Assignment2/Assignment2/ViewController.swift
+++ Assignment2/Assignment2/ViewController.swift
@@ -13,26 +13,24 @@ class MainController: UIViewController {
     @IBOutlet weak var problem3: UIButton!
     @IBOutlet weak var problem4: UIButton!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
 class Problem2: UIViewController{
-
+    
     var cells = [[Bool]](count:10, repeatedValue: Array(count:10, repeatedValue: false))
     var count = 0
-
+    
     @IBOutlet weak var textBox: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,132 +39,120 @@ class Problem2: UIViewController{
     
     
     @IBAction func button(sender: AnyObject) {
+        count = 0
+        var cells2 = [[Bool]](count:10, repeatedValue: Array(count:10, repeatedValue: false))
         for x in 0...9 {
             for y in 0...9 {
                 if arc4random_uniform(3) == 1 {
                     cells[x][y] = true
+                } else {
+                    cells[x][y] = false
                 }
                 
             }
         }
         for x in 0...9{
+            var line = ""
             for y in 0...9{
                 if cells[x][y] == true{
                     count += 1
+                    line += "x"
+                } else {
+                    line += "o"
                 }
             }
+            print ("\(line)")
+            
         }
+        print ("\(count)")
+        
+        
         var neighborCount = 0
-
+        
         for x in 0...9{
             for y in 0...9{
                 neighborCount = 0
-                for q in -1...1{
-                    for z in -1...1{
-                        if cells[(10+x+q)%10][(10+y+z)%10] == true && (q != 0 && z != 0){
+                for q in (-1)...1{
+                    for z in (-1)...1{
+                        var checkX = x + q
+                        var checkY = y + z
+                        if checkX < 0{
+                            checkX = 9
+                        }
+                        if checkY < 0{
+                            checkY = 9
+                        }
+                        if checkX > 9 {
+                            checkX = 0
+                        }
+                        if checkY  > 9 {
+                            checkY = 0
+                        }
+                        if cells[checkX][checkY] == true {
                             neighborCount += 1
-                    }
+                        }
                         
-                    if cells[x][y] == true {
-                        if neighborCount < 2 || neighborCount > 3 {
-                            cells [x][y] = false
-                        }
+                    }
+                    
 
-                        } else {
-                            if neighborCount == 3 {
-                                cells [x][y] = true
-                            }
-                        }
+                    
+                }
+                if cells[x][y] == true {
+                    neighborCount -= 1
+                }
+                print ("\(neighborCount)")
+                if cells[x][y] == true {
+                    if neighborCount < 2 || neighborCount > 3 {
+                        cells2 [x][y] = false
+                    }
+                    
+                } else {
+                    if neighborCount == 3 {
+                        cells2 [x][y] = true
                     }
                 }
+
+                
             }
         }
-        
         count = 0
         for x in 0...9{
             for y in 0...9{
-                if cells[x][y] == true{
+                if cells2[x][y] == true{
                     count += 1
                 }
             }
         }
         textBox.text = "living cells = \(count)"
         
-
+        
     }
-
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
 
 class Problem3: UIViewController{
-
-
+    var cells = [[Bool]](count:10, repeatedValue: Array(count:10, repeatedValue: false))
+    var count = 0
+    
     @IBOutlet weak var textBox: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = ("Problem 3")
     }
-    func step(cell: [[Bool]]) -> ([[Bool]]) {
-        var neighborCount = 0
-        var cells = cell
-        for x in 0...9{
-            for y in 0...9{
-                neighborCount = 0
-                for q in -1...1{
-                    for z in -1...1{
-                        if cells[(10+x+q)%10][(10+y+z)%10] == true && (q != 0 && z != 0){
-                            neighborCount += 1
-                        }
-                        
-                        if cells[x][y] == true {
-                            if neighborCount < 2 || neighborCount > 3 {
-                                cells [x][y] = false
-                            }
-                            
-                        } else {
-                            if neighborCount == 3 {
-                                cells [x][y] = true
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        
-
+    func step(cells: [[Bool]]) -> ([[Bool]]) {
         
         return cells
     }
     @IBAction func button(sender: AnyObject) {
-        var cells = [[Bool]](count:10, repeatedValue: Array(count:10, repeatedValue: false))
-        var count = 0
-        for x in 0...9 {
-            for y in 0...9 {
-                if arc4random_uniform(3) == 1 {
-                    cells[x][y] = true
-                }
-            }
-            
-        }
-        cells = step(cells)
-        for x in 0...9{
-                for y in 0...9{
-                    if cells[x][y] == true{
-                        count += 1
-                    }
-                }
-            }
-            textBox.text = "living cells = \(count)"
-        }
+           }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
 
@@ -179,13 +165,12 @@ class Problem4: UIViewController{
         super.viewDidLoad()
         self.title = ("Problem 4")
     }
-
+    
     @IBAction func button(sender: AnyObject) {
         textBox.text = "happy birthday"
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }

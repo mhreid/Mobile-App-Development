a4210b601ee1edc0e1ac254fcbc6e6831bab05eb
milkaMA <Micah@Micahs-MacBook-Pro.local>
2016-08-01 11:23:44 -0400

    Starting JSON

---
+++ FinalProject/FinalProject/ConfigCell.swift
@@ -0,0 +1,13 @@
+//
+//  ConfigCell.swift
+//  FinalProject
+//
+//  Created by Micah Reid on 7/31/16.
+//  Copyright © 2016 Micah Reid. All rights reserved.
+//
+
+import UIKit
+
+class ConfigCell: UITableViewCell {
+    
+}
